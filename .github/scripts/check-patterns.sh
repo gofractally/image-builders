6118e7ea39f017d2e1429b0a5b564cdd7708870a
James-Mart <jmartx93@gmail.com>
2024-02-12 12:10:58 -0500

    fix globbing

--- .github/scripts/check-patterns.sh
+++ .github/scripts/check-patterns.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
+# Enable globstar for recursive globbing
+shopt -s globstar
+
 ALL_CHANGED_FILES=("$@")
 
-TOOL_CONFIG_PATTERNS="docker/tool-config.Dockerfile .github/workflows/tool-config.yml docker/conf/*"
-BUILDER_2004_PATTERNS="docker/ubuntu-2004-builder.Dockerfile .github/workflows/builder-ubuntu.yml"
-BUILDER_2204_PATTERNS="docker/ubuntu-2204-builder.Dockerfile .github/workflows/builder-ubuntu.yml"
-CONTRIB_PATTERNS="docker/psibase-contributor.Dockerfile .github/workflows/contributor.yml"
-
-echo "Inside script, got changed files:"
-for file in "${ALL_CHANGED_FILES[@]}"; do
-    echo "$file"
-done
+TOOL_CONFIG_PATTERNS=("docker/tool-config.Dockerfile .github/workflows/tool-config.yml docker/conf/**")
+BUILDER_2004_PATTERNS=("docker/ubuntu-2004-builder.Dockerfile .github/workflows/builder-ubuntu.yml")
+BUILDER_2204_PATTERNS=("docker/ubuntu-2204-builder.Dockerfile .github/workflows/builder-ubuntu.yml")
+CONTRIB_PATTERNS=("docker/psibase-contributor.Dockerfile .github/workflows/contributor.yml")
 
 matches_pattern() {
     local pattern="$1"
-    for file in "${ALL_CHANGED_FILES[@]}"; do
-        if [[ "$file" == $pattern ]]; then
+    for file in ${ALL_CHANGED_FILES[@]}; do
+        if [[ $file == $pattern ]]; then
             return 0 # Success
         fi
     done
@@ -30,23 +28,23 @@ matches_pattern() {
 # 5: run contributor 
 # 0: don't run anything
 
-for pattern in "${TOOL_CONFIG_PATTERNS[@]}"; do
-    if matches_pattern "$pattern"; then
+for pattern in ${TOOL_CONFIG_PATTERNS[@]}; do
+    if matches_pattern $pattern; then
         echo 1
         exit
     fi
 done
 
 run_2004=false
-for pattern in "${BUILDER_2004_PATTERNS[@]}"; do
-    if matches_pattern "$pattern"; then
+for pattern in ${BUILDER_2004_PATTERNS[@]}; do
+    if matches_pattern $pattern; then
         run_2004=true
     fi
 done
 
 run_2204=false
-for pattern in "${BUILDER_2204_PATTERNS[@]}"; do
-    if matches_pattern "$pattern"; then
+for pattern in ${BUILDER_2204_PATTERNS[@]}; do
+    if matches_pattern $pattern; then
         run_2204=true
     fi
 done
@@ -62,8 +60,8 @@ elif $run_2204; then
     exit
 fi
 
-for pattern in "${CONTRIB_PATTERNS[@]}"; do
-    if matches_pattern "$pattern"; then
+for pattern in ${CONTRIB_PATTERNS[@]}; do
+    if matches_pattern $pattern; then
         echo 5
         exit
     fi
